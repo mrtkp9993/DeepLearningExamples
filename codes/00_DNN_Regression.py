ec03b8e337aed38494333d559cc6ca302d523b8e
Murat Koptur <mkoptur3@gmail.com>
2020-11-09 18:09:25 +0300

    Update 00_DNN_Regression.py

--- codes/00_DNN_Regression.py
+++ codes/00_DNN_Regression.py
@@ -2,10 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.utils import plot_model
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.utils import plot_model
 
 # Read dataset
 df = pd.read_csv('data/SeoulBikeData.csv',
